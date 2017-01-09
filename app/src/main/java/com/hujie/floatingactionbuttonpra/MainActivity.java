75eebc862f0aeb2a76bf3b2dabcb1126412841e8
hujie <380211629@qq.com>
2017-01-09 22:25:02 +0800

    FloatingActionBar practice

--- app/src/main/java/com/hujie/floatingactionbuttonpra/MainActivity.java
+++ app/src/main/java/com/hujie/floatingactionbuttonpra/MainActivity.java
@@ -1,15 +1,20 @@
 package com.hujie.floatingactionbuttonpra;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +31,82 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        View view = findViewById(R.id.bottom_action);
+        final BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(view);
+
+        findViewById(R.id.show).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //展开
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        findViewById(R.id.dismiss).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //隐藏
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
+
+        findViewById(R.id.collapsed).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //收缩
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+
+        findViewById(R.id.expanded).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //展开
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+        findViewById(R.id.dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //创建Dialog
+                sheetDialog.show();
+            }
+        });
+
+        createDialog();
+    }
+    BottomSheetDialog sheetDialog;
+    public void createDialog(){
+        sheetDialog=new BottomSheetDialog(MainActivity.this);
+        sheetDialog.setContentView(R.layout.content_main);//设置显示的布局
+
+        //1 获得Dialog的View
+        AppCompatDelegate rootView = sheetDialog.getDelegate();
+        //2 BottomSheetBahavior
+        View view = rootView.findViewById(android.support.design.R.id.design_bottom_sheet);
+        final BottomSheetBehavior<View> bottomSheetBehavior = BottomSheetBehavior.from(view);
+
+        //检测 BottomSheetBahavior 状态
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            //状态变化的时候回调
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                //往下滑动隐藏，状态STATE_HIDDEN  处理隐藏的状态
+                if(newState==BottomSheetBehavior.STATE_HIDDEN){
+                    sheetDialog.dismiss();
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+            //手指拖动的时候调用
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
